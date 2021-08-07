eef12c1d5bb44e04de5a5c839da5d29d748bc408
Steven Tan <steven@steventan.com.au>
2021-08-07 10:51:35 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
+// const colours = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
@@ -7,21 +8,21 @@ module.exports = {
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans]
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
       }
-    },
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
   plugins: [
-    require("@tailwindcss/ui"),
+    require('@tailwindcss/ui'),
     require('@tailwindcss/forms')
-  ],
+  ]
 }
